7f54d47a35c8a74fb3c32a2bd3bb0bd471e0fcb2
C42xe2 <erikdonath2006@gmail.com>
2024-06-07 13:00:08 +0200

    Changed Server Skript.

--- server.py
+++ server.py
@@ -2,16 +2,15 @@ from wsgiref.simple_server import make_server
 from app import app
 
 
-# Diese Funktion wird als WSGI-Anwendung verwendet
 def application(environ, start_response):
-    # Rufe die Flask-Anwendung auf
     response = app(environ, start_response)
     return response
 
 
 if __name__ == '__main__':
-    host = '10.254.1.223'  # Host, auf dem der Server lauschen soll (alle verfügbaren Netzwerkschnittstellen)
-    port = 80      # Port, auf dem der Server lauschen soll
+    host = '10.254.1.223'
+    port = 80
+
     httpd = make_server(host, port, application)
     print(f"Server on {host}:{port}")
     httpd.serve_forever()
